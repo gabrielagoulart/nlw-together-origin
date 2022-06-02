3e7918f60b27794f16e89b1305651691879d8f3c
Gabriela Goulart <101572408+gabrielagoulart@users.noreply.github.com>
2022-06-01 21:29:33 -0300

    scroll menu

--- main.js
+++ main.js
@@ -16,3 +16,15 @@ for(const link of links) {
         nav.classList.remove('show')
     })
 }
+
+//MUDAR O HEADER DA PAGINA QUANDO DER SCROLL
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+    if(window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+    } else {
+       header.classList.remove('scroll')
+    }
+})
