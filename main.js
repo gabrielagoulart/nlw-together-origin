811ec23ae68f875408f8da2867e08eb29fd78959
Gabriela Goulart <101572408+gabrielagoulart@users.noreply.github.com>
2022-06-05 18:52:03 -0300

    contact e scollreveal

--- main.js
+++ main.js
@@ -39,3 +39,19 @@ const swiper = new Swiper('.swiper-container', {
     mousewheel: true,
     keyboard: true
 })
+
+//MOSTRAR ELEMENTOS AO DAR SCOLL NA PAGINA
+const scrollReveal = ScrollReveal ({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text,
+     #about .image, #about .text,
+     #services header, #services .card,
+     #testimonials header, #testimonials .testimonials,
+     #contact .text, #contact .links
+`, { interval:100})
